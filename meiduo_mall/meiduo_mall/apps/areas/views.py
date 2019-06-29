d5b32364b8cd7a72be852da725e98875accb0a6a
杨浪 <578599180@qq.com>
2019-06-29 11:59:42 +0800

    '实验3'

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -15,17 +15,17 @@ from .serializers import AreaSerializer, SubsSerializer
 
 
 # Create your views here.
-# class AreaListView(APIView):
-#     """查询所有省"""
-#
-#     def get(self, request):
-#         # 1.获取指定的查询集
-#         qs = Area.objects.filter(parent=None)
-#         # qs = Area.objects.all()
-#         # 2.创建序列化器进行序列化
-#         serializer = AreaSerializer(qs, many=True)
-#         # 3.响应
-#         return Response(serializer.data)
+class AreaListView(APIView):
+    """查询所有省"""
+
+    def get(self, request):
+        # 1.获取指定的查询集
+        qs = Area.objects.filter(parent=None)
+        # qs = Area.objects.all()
+        # 2.创建序列化器进行序列化
+        serializer = AreaSerializer(qs, many=True)
+        # 3.响应
+        return Response(serializer.data)
 #
 #
 # class AreaDetailView(APIView):
