627a1043e04f414b593af70b4bdb89c67f6af627
darcy <yanjiabinchn@gmail.com>
2019-06-29 11:54:15 +0800

    实验

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -6,26 +6,44 @@ from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework_extensions.cache.mixins import CacheResponseMixin
-
 from .models import Area
 from .serializers import AreaSerializer, SubsSerializer
 
-from .models import Area
-from .serializers import AreaSerializer, SubsSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Create your views here.
-# class AreaListView(APIView):
-#     """查询所有省"""
-#
-#     def get(self, request):
-#         # 1.获取指定的查询集
-#         qs = Area.objects.filter(parent=None)
-#         # qs = Area.objects.all()
-#         # 2.创建序列化器进行序列化
-#         serializer = AreaSerializer(qs, many=True)
-#         # 3.响应
-#         return Response(serializer.data)
+class AreaListView(APIView):
+    """查询所有省"""
+
+    def get(self, request):
+        # 1.获取指定的查询集
+        qs = Area.objects.filter(parent=None)
+        # qs = Area.objects.all()
+        # 2.创建序列化器进行序列化
+        serializer = AreaSerializer(qs, many=True)
+        # 3.响应
+        return Response(serializer.data)
 #
 #
 # class AreaDetailView(APIView):
